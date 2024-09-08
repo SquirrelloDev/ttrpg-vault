1ba34a629af1b3a7756c791304e7ee551634a8e5
Emil Olejnik <main@emilolejnik@gmail.com>
2024-09-08 15:18:05 +0200

    chore: add configuration for path aliases; add react-router

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -8,5 +9,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./setupTests.ts']
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
   }
 })
