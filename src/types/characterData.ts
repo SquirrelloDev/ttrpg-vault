dffed7aa27201b870d57f6cfb38d2bb2a3408fee
Emil Olejnik <olejnike2000@gmail.com>
2024-09-11 14:01:05 +0200

    Chore: add character data

---
+++ src/types/characterData.ts
@@ -0,0 +1,19 @@
+type CharacterData = {
+    characterName: string
+    age: number
+    job: string
+    gender: "M" | "K"
+    domicile: string
+    placeOfBirth: string
+    description: string
+    ideology: string
+    importantPeople: string
+    importantPlaces: string
+    personalBelongings: string
+    perks: string
+    scars: string
+    phobias: string
+    mysteryItems: string
+    strangeEncounters: string
+}
+export default CharacterData
---
+++ src/types/characterTraits.ts
@@ -0,0 +1,19 @@
+type BasicTraits = {
+    strength: number
+    constitution: number
+    dexterity: number
+    intelligence: number
+    apperance: number
+    size: number
+    education: number
+}
+type damageModValues = number | "1K4" | "1K6"
+type DerivedTraits = {
+    healthPoints: number
+    sanityPoints: number
+    magicPoints: number
+    luckPoints: number
+    damageMod: damageModValues
+    build: number
+}
+export type CharaterTraits = BasicTraits & DerivedTraits
