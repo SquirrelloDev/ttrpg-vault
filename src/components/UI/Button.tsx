4e57cf3ebd032c0c61996da52059812f755014a6
Emil Olejnik <olejnike2000@gmail.com>
2024-10-07 14:18:00 +0200

    Styling: add extra styling possibilities for the button component

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,12 +1,14 @@
+import clsx from "clsx";
 import { ButtonHTMLAttributes } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   onClickHandler: () => void;
+  classNames?: string
 }
-function Button({ children, onClickHandler, ...props }: ButtonProps) {
+function Button({ children, onClickHandler, classNames, ...props }: ButtonProps) {
   return (
     <button
-      className="text-lime-800 font-kalam text-lg bg-neutral-950 my-2 py-3 px-6 border border-lime-800 rounded-lg hover:text-black hover:bg-lime-800 transition-colors"
+      className={clsx("text-lime-800 font-kalam text-lg bg-neutral-950 my-2 py-3 px-6 border border-lime-800 rounded-lg hover:text-black hover:bg-lime-800 transition-colors", classNames)}
       onClick={onClickHandler}
       {...props}
     >
